419ed6c3ba3509398b15a12de1577ceaaef4e6b0
NB InfoTech <nbinfotech24@gmail.com>
2026-07-26 11:37:29 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "./",
+  base: '/SpecterJS/',
   build: {
-    outDir: "dist",
-    sourcemap: true,
+    outDir: 'dist',
   },
 });
